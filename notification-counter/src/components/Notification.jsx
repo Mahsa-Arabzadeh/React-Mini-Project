5a248c4ab7117bdbb8fcedb4bf8f6306e3c9cabd
Mahsa <arabzadehmah@gmail.com>
2024-03-18 03:16:41 +0330

    Fix getNotification methode.

--- notification-counter/src/components/Notification.jsx
+++ notification-counter/src/components/Notification.jsx
@@ -4,13 +4,40 @@ import "./Notification.css"
 import { Component } from 'react'
 
 export default class Notification extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            Notifications: [
+              {telegram: 0},
+              {instagram: 0},
+              {whatsapp: 0},
+            ]
+          }
+
+          this.getNotification = this.getNotification.bind(this)
+
+    }
+
+
+    getNotification(){
+        if(this.state.Notifications.length > 0){
+          return this.state.Notifications.length
+        } 
+        else{
+          return "null"
+        }   
+    }
+
+
   render() {
     return (
       <div>
         <header className="header">
             <div className="logo">SH</div>
             <div className="notif-container">
-                <div className="notif-box">1</div>
+                <div className="notif-box">{this.getNotification()}</div>
                 <div className="news">Telegram</div>
             </div>
         </header>
@@ -18,3 +45,4 @@ export default class Notification extends Component {
     )
   }
 }
+
